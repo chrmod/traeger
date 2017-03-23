369866c2d1621012d2ad9e179203bb0900456640
Krzysztof Jan Modras <chrmod@chrmod.net>
2017-03-24 00:18:39 +0100

    listing dependencies

---
+++ src/main.rs
@@ -0,0 +1,11 @@
+extern crate webextension_rust_template as protocol;
+use std::io;
+
+fn main() {
+    loop {
+        let f = protocol::Input::Stdin(io::stdin());
+        let message = protocol::read(f);
+        let output = protocol::Output::Stdout(io::stdout());
+        protocol::write(output, message.to_string());
+    }
+}
