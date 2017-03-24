e17bef38b8e0bd5d825c737bcb535508e9c338fd
Krzysztof Jan Modras <chrmod@chrmod.net>
2017-03-24 23:16:45 +0100

    separate thread reads from stdin

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,23 @@
 extern crate webextension_rust_template as protocol;
 use std::io;
+use std::thread;
+use std::sync::mpsc;
 
 fn main() {
+    let (tx, rx) = mpsc::channel();
+    let sender = tx.clone();
+
+    // thread reads from stdin and send messages to main thread
+    thread::spawn(move || {
+        loop {
+            let f = protocol::Input::Stdin(io::stdin());
+            let message = protocol::read(f);
+            sender.send(message).unwrap();
+        }
+    });
+
     loop {
-        let f = protocol::Input::Stdin(io::stdin());
-        let message = protocol::read(f);
+        let message = rx.recv().unwrap();
         let output = protocol::Output::Stdout(io::stdout());
         protocol::write(output, message.to_string());
     }
