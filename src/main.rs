e382fc50a9fb57e25f5e1c08316af00ac2a4b8b0
Krzysztof Jan Modras <chrmod@chrmod.net>
2017-03-26 21:31:29 +0200

    updating to new webextension-protocol version

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
-extern crate webextension_rust_template as protocol;
-use std::io;
+#[macro_use(println_stderr)]
+extern crate webextension_protocol as protocol;
+use std::io::Write;
 use std::thread;
 use std::sync::mpsc;
 
@@ -10,15 +11,17 @@ fn main() {
     // thread reads from stdin and send messages to main thread
     thread::spawn(move || {
         loop {
-            let f = protocol::Input::Stdin(io::stdin());
-            let message = protocol::read(f);
+            let message = match protocol::read_stdin() {
+                Ok(m) => m,
+                Err(_) => panic!("nothing more to read"),
+            };
             sender.send(message).unwrap();
         }
     });
 
     loop {
         let message = rx.recv().unwrap();
-        let output = protocol::Output::Stdout(io::stdout());
-        protocol::write(output, message.to_string());
+        println_stderr!("received {}", message);
+        protocol::write_stdout(message);
     }
 }
