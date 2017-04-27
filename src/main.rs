7a767b59ab8cf49ffd1d5f7e4cf2a1a4067a450a
Krzysztof Jan Modras <chrmod@chrmod.net>
2017-04-27 20:19:08 +0200

    server cleanup

--- src/main.rs
+++ src/main.rs
@@ -2,25 +2,20 @@
 
 extern crate rustc_serialize;
 extern crate libc;
-
 #[macro_use(println_stderr)]
 extern crate webextension_protocol as protocol;
-use std::io::Write;
-
 #[macro_use]
 extern crate js;
-
-use std::thread;
-use std::sync::mpsc;
-
-use std::net::{TcpListener};
-
-use server::SocksServer;
-use js_engine::JsEngine;
 
 mod js_engine;
 mod server;
 mod helpers;
+
+use std::io::Write;
+use std::thread;
+use std::sync::mpsc;
+use server::{ServerWrapper};
+use js_engine::JsEngine;
 
 #[allow(unused_must_use)]
 fn main() {
@@ -45,30 +40,8 @@ fn main() {
     let sender_for_http = tx.clone();
 
     thread::spawn(move || {
-        let listener = TcpListener::bind(("127.0.0.1", 0)).unwrap();
-        match listener.local_addr() {
-            Ok(socket_name) => {
-                let port = socket_name.port();
-                let message = helpers::js_message("setPort".to_string(), port.to_string());
-                helpers::send_async(sender_for_http.clone(), message);
-                println_stderr!("Listening on: {}", socket_name);
-            },
-            Err(_) => panic!("cannot aquire port"),
-        }
-
-        loop {
-            match listener.accept() {
-                Err(e) => {
-                    println_stderr!("There was an error omg {}", e)
-                }
-                Ok((stream, _)) => {
-                    let sender_for_http = sender_for_http.clone();
-                    thread::spawn(move || {
-                        SocksServer::new(stream, sender_for_http);
-                    });
-                }
-            }
-        }
+        let mut server = ServerWrapper::new(sender_for_http);
+        server.start();
     });
 
     let mut js_engine = JsEngine::new();
